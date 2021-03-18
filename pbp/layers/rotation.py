03314a1dd236c6f17618c203e0a60cf22d75eddc
Angelos Katharopoulos <katharas@gmail.com>
2021-03-18 19:24:40 +0100

    Add Gram-Schmidt for rotation prediction

--- pbp/layers/rotation.py
+++ pbp/layers/rotation.py
@@ -43,16 +43,18 @@ class SVDO(torch.nn.Module):
         enforce_rotation: bool, if set to True ensures that the determinant of
                           the predicted matrix will be 1 thus a valid rotation
                           and not a roto-reflection (default: True)
+        out_dims: int, the dimensionality of the rotation matrix (default: 3)
     """
-    def __init__(self, input_dims, enforce_rotation=True):
+    def __init__(self, input_dims, enforce_rotation=True, out_dims=3):
         super().__init__()
 
         self.enforce_rotation = enforce_rotation
-        self.linear = torch.nn.Linear(input_dims, 9)
+        self.out_dims = out_dims
+        self.linear = torch.nn.Linear(input_dims, out_dims*out_dims)
 
     def forward(self, x):
         shape = x.shape[:-1]
-        M = self.linear(x).view(*(shape + (3, 3)))
+        M = self.linear(x).view(*(shape + (self.out_dims, self.out_dims)))
 
         U, S, V = torch.svd(M)
 
@@ -64,3 +66,34 @@ class SVDO(torch.nn.Module):
             U = U * S_hat[..., None, :]
 
         return torch.einsum("...ik,...jk->...ij", U, V)
+
+
+class GS3(torch.nn.Module):
+    """Perform Gram-Schmidt orthogonalization for 3x3 matrices, namely the 6D
+    representation from [1].
+
+    [1]: Zhou, Y., Barnes, C., Lu, J., Yang, J. and Li, H., 2019. On the
+         continuity of rotation representations in neural networks. CVPR 2019.
+
+    Arguments
+    ---------
+        input_dims: int, the dimensionality of the input feature
+    """
+    def __init__(self, input_dims):
+        super().__init__()
+        self.linear = torch.nn.Linear(input_dims, 6)
+
+    def forward(self, x):
+        u = self.linear(x)
+        u1 = u[..., :3]
+        u2 = u[..., 3:]
+
+        u1_norm = (u1*u1).sum(-1)
+        u2 = u2 - ((u1*u2).sum(-1) / u1_norm)[..., None] * u1
+        u2_norm = (u2*u2).sum(-1)
+
+        e1 = u1 / torch.sqrt(u1_norm)[..., None]
+        e2 = u2 / torch.sqrt(u2_norm)[..., None]
+        e3 = torch.cross(e1, e2, dim=-1)
+
+        return torch.stack([e1, e2, e3], dim=-2)
