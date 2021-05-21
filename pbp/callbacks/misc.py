f6c0e71dff469bc1bc24849e9906c899ba20a01d
Angelos Katharopoulos <angelos.katharopoulos@idiap.ch>
2021-05-21 22:14:48 +0200

    Add a callback for cooperative scheduling
    Basically we add a callback that exits the experiment early but with a non-zero
    exit code in order for the scheduler to be able to reschedule the job.

--- pbp/callbacks/misc.py
+++ pbp/callbacks/misc.py
@@ -2,6 +2,7 @@ import json
 import os
 from os import path
 from subprocess import run
+import sys
 
 from .base import Callback
 
@@ -48,3 +49,46 @@ class LogArguments(Callback):
             os.getcwd(),
             __main__.__file__
         )
+
+
+class CooperativeGridScheduling(Callback):
+    """Exit with a non-zero exit code after a given amount of epochs or
+    iterations in order to allow the scheduler to rebalance.
+
+    NOTE: This callback does not resubmit the job to the scheduler. This should
+          be done externally.
+
+    Arguments
+    ---------
+        yield_after_iterations: int, exit after that many training iterations
+                                (default: 0)
+        yield_after_epochs: int, exit after that many epochs (default: 0)
+        exit_code: int, which exit code to use when exiting (default: 1)
+    """
+    def __init__(self, yield_after_iterations:int = 0,
+                 yield_after_epochs:int = 0, exit_code:int = 1):
+        self.yield_after_iterations = yield_after_iterations
+        self.yield_after_epochs = yield_after_epochs
+        self.exit_code = exit_code
+
+        self._iterations = 0
+        self._epochs = 0
+
+    def _yield(self):
+        sys.exit(self.exit_code)
+
+    def on_train_batch_stop(self, experiment):
+        if self.yield_after_iterations <= 0:
+            return
+
+        self._iterations += 1
+        if self._iterations >= self.yield_after_iterations:
+            self._yield()
+
+    def on_epoch_start(self, experiment):
+        if self.yield_after_epochs <= 0:
+            return
+
+        self._epochs += 1
+        if self._epochs > self.yield_after_epochs:
+            self._yield()
