666280d9a39836c063a798aea96e7f49cf995209
Angelos Katharopoulos <katharas@gmail.com>
2021-05-22 12:40:15 +0200

    Change the way wandb logs when per_epoch is False

--- pbp/callbacks/wandb.py
+++ pbp/callbacks/wandb.py
@@ -22,17 +22,19 @@ class WandB(Logger):
         wandb_watch: bool, use wandb.watch() on the model (default: True)
         wandb_per_epoch: bool, log the metrics per epoch or per update
                          (default: True)
-        wandb_log_frequency: int, the log frequency passed to wandb.watch
-                             (default: 10)
+        wandb_log_frequency: int, log every that many batches (default: 10)
+        wandb_watch_log_frequency: int, the log frequency passed to wandb.watch
+                                   (default: 1000)
         wandb_run_id: str, the run id in order to resume a previously preempted
                       run (default: '')
 """
     def __init__(self, wandb_project:str = "", wandb_watch:bool = True,
                  wandb_per_epoch:bool = True, wandb_log_frequency:int = 10,
-                 wandb_run_id:str = ""):
+                 wandb_watch_log_frequency:int = 1000, wandb_run_id:str = ""):
         self.project = wandb_project
         self.watch = wandb_watch
         self.log_frequency = wandb_log_frequency
+        self.watch_log_frequency = wandb_watch_log_frequency
         self.per_epoch = wandb_per_epoch
         self.run_id = wandb_run_id
         self._values = defaultdict(AverageMeter)
@@ -53,7 +55,7 @@ class WandB(Logger):
         )
 
         if self.watch:
-            wandb.watch(experiment.model, log_freq=self.log_frequency)
+            wandb.watch(experiment.model, log_freq=self.watch_log_frequency)
 
     def log(self, key, value):
         self._values[key] += value
@@ -62,26 +64,25 @@ class WandB(Logger):
         if self.per_epoch:
             return
 
-        self._values["step"] += experiment.trainer.current_steps
-        wandb.log({k: v.current_value for (k, v) in self._values.items()})
-        self._values.clear()
-
-    def on_val_batch_stop(self, experiment):
-        if self.per_epoch:
+        if experiment.trainer.current_steps % self.log_frequency != 0:
             return
 
-        self._values["val_step"] += self._validation_batches
-        wandb.log({k: v.current_value for (k, v) in self._values.items()})
+        self._values["step"] += experiment.trainer.current_steps
+        wandb.log({k: v.average_value for (k, v) in self._values.items()})
         self._values.clear()
-        self._validation_batches += 1
 
     def on_epoch_start(self, experiment):
-        if not self.per_epoch:
-            return
-
         if len(self._values) == 0:
             return
 
         self._values["epoch"] += experiment.trainer.current_epoch-1
+        wandb.log({k: v.average_value for (k, v) in self._values.items()})
+        self._values.clear()
+
+    def on_train_stop(self, experiment):
+        if len(self._values) == 0:
+            return
+
+        self._values["epoch"] += experiment.trainer.current_epoch
         wandb.log({k: v.average_value for (k, v) in self._values.items()})
         self._values.clear()
